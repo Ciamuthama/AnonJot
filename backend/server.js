c5927d39b719353a15e394c7a9a9ab855980c696
Peter Muthama <114055846+Ciamuthama@users.noreply.github.com>
2023-07-15 15:58:39 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,25 +9,25 @@ const cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 3000;
-//const dbURL = "mongodb+srv://notes:HHzwywmDBtajdvMX@notes.gsh9r2z.mongodb.net/?retryWrites=true&w=majority";
-//mongoose.connect(dbURL)
-//.then((result) => {
-    //console.log('connected to database')
-    //console.log("connected to server 3000")
-//}).catch((err) => {
-   // console.log(err)
-//});
+const dbURL = "mongodb+srv://notes:HHzwywmDBtajdvMX@notes.gsh9r2z.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(dbURL)
+.then((result) => {
+    app.listen(port)
+    console.log('connected to database')
+    console.log("connected to server 3000")
+}).catch((err) => {
+    console.log(err)
+});
 
-app.listen(port)
 app.use(express.json());
 app.use(cors({
     origin: ['https://anonjot.vercel.app'],
     methods: ['POST','GET'],
     credentials: true
     })) 
-//app.use(morgan('dev'));
+app.use(morgan('dev'));
 app.get('/', (req,res)=>{
-     res.json('hello');
+     res.json('hello').redirect('/');
  });
 
 app.use('/', notesRouter)
