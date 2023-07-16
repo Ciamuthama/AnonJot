5d65b122877e4a3dbb2f52a2b797296618f8592f
Peter Muthama <114055846+Ciamuthama@users.noreply.github.com>
2023-07-16 17:13:09 +0300

    Update index.jsx

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -12,7 +12,7 @@ function Notes() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://anonjotapi.vercel.app/notes', { cache: true });
+        const response = await axios.get('https://anonjotapi.vercel.app/notes', { cache: true });
         setContent(response.data);
         setLoading(false)
       } catch (error) {
