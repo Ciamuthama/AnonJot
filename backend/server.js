cdcb34024d7e63722d8092c3dfd8004c673507c5
Peter Muthama <114055846+Ciamuthama@users.noreply.github.com>
2023-07-16 13:57:49 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ console.log('connected to database')
 });
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://anonjot.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
