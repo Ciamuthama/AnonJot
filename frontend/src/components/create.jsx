1256e9084c36e7e53994f4e563880a909bb16dbf
Peter Muthama <114055846+Ciamuthama@users.noreply.github.com>
2023-07-16 11:48:42 +0300

    Update create.jsx api link extension

--- frontend/src/components/create.jsx
+++ frontend/src/components/create.jsx
@@ -25,7 +25,7 @@ function New() {
     }
     
     axios
-      .post("https://anonjotapi.vercel.app/new", note)
+      .post("https://anonjotapi.vercel.app/notes/new", note)
       .then((res) => res.data,
       )
       .catch((err)=> err)
