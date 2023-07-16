8d7921ad139eede830d4725209ea41be6079c420
Peter Muthama <114055846+Ciamuthama@users.noreply.github.com>
2023-07-16 12:13:57 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   next();
 }); 
 app.use(morgan('dev'));
-app.get('/', (req,res)=>{
+app.get('/notes', (req,res)=>{
      res.json("hello").redirect('/notes');
  });
 
