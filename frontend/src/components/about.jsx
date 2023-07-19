8b6708d3123d3edd82d590af32bf5f4cf72c0588
Peter Muthama <114055846+Ciamuthama@users.noreply.github.com>
2023-07-19 10:31:49 +0300

    Update about.jsx

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -1,7 +1,8 @@
 function About() {
     return ( 
         <>
-        <div> created and maintained by Pete </div>
+        <div> Welcome to Anon Jot, a unique MERN Stack project that allows you to share notes, thoughts, and ideas anonymously with ease! 📝💭 This web app provides a user-friendly experience, eliminating the need to sign in to create a new note. Rest assured, your data remains private, making Anon Jot a safe and enjoyable platform to express yourself. 😊
+            Created and maintained by <a href='https://twitter.com/Caimuthama'>Peter</a> </div>
         </>
      );
 }
