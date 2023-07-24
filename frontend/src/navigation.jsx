7cc4bf503fd9012eeb2ffb665860fb0569f595b9
Peter Muthama <114055846+Ciamuthama@users.noreply.github.com>
2023-07-24 23:51:24 +0300

    Update navigation.jsx

--- frontend/src/navigation.jsx
+++ frontend/src/navigation.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 function Navbar() {
 	return (
 		<>
-			<nav>
-				<ul>
-					<li>
-						<Link to="/">
+		<nav>
+			<ul>
+				<li>
+					<Link to="/">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								width="24"
@@ -17,8 +17,7 @@ function Navbar() {
 								strokeWidth="2"
 								strokeLinecap="round"
 								strokeLinejoin="round"
-								className="feather feather-home"
-							>
+								className="feather feather-home" >
 								<path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
 								<polyline points="9 22 9 12 15 12 15 22" />
 							</svg>
@@ -36,8 +35,7 @@ function Navbar() {
 								strokeWidth="2"
 								strokeLinecap="round"
 								strokeLinejoin="round"
-								className="feather feather-plus-circle"
-							>
+								className="feather feather-plus-circle">
 								<circle cx="12" cy="12" r="10" />
 								<line x1="12" y1="8" x2="12" y2="16" />
 								<line x1="8" y1="12" x2="16" y2="12" />
@@ -56,8 +54,7 @@ function Navbar() {
 								strokeWidth="2"
 								strokeLinecap="round"
 								strokeLinejoin="round"
-								className="feather feather-help-circle"
-							>
+								className="feather feather-help-circle">
 								<circle cx="12" cy="12" r="10" />
 								<path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
 								<line x1="12" y1="17" x2="12.01" y2="17" />
