6a18cf8267d7ab7f0afc84471ddcc042fdcdd4b0
Ciamuthama <muthamapeter79@gmail.com>
2023-07-31 10:34:04 +0300

    Fixed bugs in tailwind css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
-module.exports = {
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [  ],
+  plugins: [],
 }
+
