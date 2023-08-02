77a3c8f4f768e7ad2783b700a7ed55c156668f97
Ciamuthama <muthamapeter79@gmail.com>
2023-08-02 11:18:43 +0300

    Changed the height of the textarea to 260px and changed the button size to btn-xs, sm:btn-sm, md:btn-md, lg:btn-lg.

--- frontend/src/components/create.jsx
+++ frontend/src/components/create.jsx
@@ -51,14 +51,15 @@ function New() {
           onChange={onTitleChange}
         />
           <textarea
-         rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          rows="4" className="block p-2.5 w-full h-screen text-sm text-gray-900 bg-primary-content rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-8"
+          style={'height:260px'}
           name="body"
           id="body"
           placeholder="Note"
           value={body}
           onChange={onBodyChange}
           />
-        <button type="submit" className="btn">Submit</button>
+        <button type="submit" className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Submit</button>
       </form>
     </>
   );
