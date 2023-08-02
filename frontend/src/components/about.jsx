1e3199993003f592d981ab023988f9ef039e2b1f
Ciamuthama <muthamapeter79@gmail.com>
2023-08-02 12:30:59 +0300

    Added a margin to the about section.

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -1,8 +1,10 @@
 function About() {
     return ( 
         <>
+        <div className="m-8">
         <div> Welcome to Anon Jot, a unique MERN Stack project that allows you to share notes, thoughts, and ideas anonymously with ease! 📝💭 This web app provides a user-friendly experience, eliminating the need to sign in to create a new note. Rest assured, your data remains private, making Anon Jot a safe and enjoyable platform to express yourself. 😊
-            Created and maintained by <a href='https://twitter.com/CiaMuthama'>Peter</a> </div>
+            Created and maintained by <a className="link link-info" href='https://twitter.com/CiaMuthama'>Peter</a> </div>
+        </div>
         </>
      );
 }
