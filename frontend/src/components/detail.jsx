abc3c26fa6cc98262b34ca4dfb3d073b6e2c59da
Ciamuthama <muthamapeter79@gmail.com>
2023-08-02 14:08:38 +0300

    Added useNavigate to navigate to the previous page after deleting a note.

--- frontend/src/components/detail.jsx
+++ frontend/src/components/detail.jsx
@@ -1,14 +1,15 @@
 // /* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Loader } from "./Loader";
 
 function Details() {
   const [details, setDetails] = useState([]);
   const [loading,setLoading] = useState(true)
   const params = useParams()
-  
+  const navigate = useNavigate()
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -36,7 +37,8 @@ function Details() {
     axios.delete(`https://anonjotapi.vercel.app/notes/${params.id}`)
       .then(() => {
           setDetails(details.filter((details) => details.id !== details.id));
-        })
+      })
+    navigate('..')
     }
     
     if(loading) return(< Loader />)
@@ -48,7 +50,7 @@ function Details() {
            <h1> {details.title} </h1>
            <p> {details.body}</p>
        </div>
-       <div onClick={()=>window.location.href='/'}>
+       <div className="flex justify-end">
          <button onClick={()=> handleRemove(details.id)} className="btn btn-md md:btn-md lg:btn-lg btn-error">Delete</button>
        </div>
          
