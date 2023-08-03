5a96febbc4222d32ee5f9c01a01b8c47bcff5e32
Ciamuthama <muthamapeter79@gmail.com>
2023-08-03 12:04:55 +0300

    Updated App.jsx to use Navigate instead of Route for the default path.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Notes from "./components/index";
 import Details from "./components/detail";
 import New from "./components/create";
 import About from "./components/about";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 
 
@@ -21,10 +21,8 @@ function App() {
           <Route path="/detail/:id" Component={Details} />
           <Route path="/new" Component={New} />
           <Route path="/about" Component={About} />
-          {/* <Route path="*" >
-            <Navigate to='/' />
-          </Route> */}
-        </Routes>
+          <Route path="*" element={<Navigate to='/' />} />
+          </Routes>
       </Router>
     </>
   );
