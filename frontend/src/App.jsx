083065be524cb78a2b23c74ad2e4c655f4320437
Peter Muthama <114055846+Ciamuthama@users.noreply.github.com>
2024-01-22 11:33:49 +0300

    Removed the details route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@
 
 import Navbar from "./navigation";
 import Notes from "./components/index";
-import Details from "./components/detail";
 import New from "./components/create";
 import About from "./components/about";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
@@ -19,7 +18,6 @@ function App() {
         <br />
         <Routes>
           <Route exact path="/" Component={Notes} />
-          <Route path="/detail/:id" Component={Details} />
           <Route path="/new" Component={New} />
           <Route path="/about" Component={About} />
           <Route path="*" element={<Navigate to='/' />} />
