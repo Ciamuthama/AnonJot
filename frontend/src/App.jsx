89ec0b4a39da7be67d4aa3ef80eadbac6d49dec9
Peter Muthama <114055846+Ciamuthama@users.noreply.github.com>
2024-01-22 11:40:39 +0300

    Reverted the changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@
 import Navbar from "./navigation";
 import Notes from "./components/index";
 import New from "./components/create";
+import Details from "./components/detail";
 import About from "./components/about";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
@@ -19,6 +20,7 @@ function App() {
         <Routes>
           <Route exact path="/" Component={Notes} />
           <Route path="/new" Component={New} />
+          <Route path ="/detail/:id" Component={Details}/>
           <Route path="/about" Component={About} />
           <Route path="*" element={<Navigate to='/' />} />
           </Routes>
